7d0798ad1eba4dc89c45d5699441bd3bee12c9c0
MiregX <markgerasimchuk8@gmail.com>
2025-10-13 02:14:14 +0200

    _

--- client/src/middleware/auth-middleware.ts
+++ client/src/middleware/auth-middleware.ts
@@ -6,13 +6,20 @@ export async function authMiddleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const token = req.cookies.get(Authorization);
 
-  const isLoggedIn = token ? await fetch('http://localhost:1337/api/auth/profile', {
-    method: 'GET',
-    headers: {
-      [Authorization]: token.value
-    },
-    next: { tags: ['profile'] }
-  }).then(response => response.json().then(payload => typeof payload.data.id !== 'undefined' && payload.data.email === 'ceo@impactium.dev')) : false;
+  const isLoggedIn = token
+    ? await fetch(`http://${process.env.NODE_ENV === 'production' ? 'api:1337' : 'localhost:1337'}/api/auth/profile`, {
+      method: 'GET',
+      headers: {
+        [Authorization]: token.value
+      },
+      next: { tags: ['profile'] }
+    })
+    .then(async response => {
+      const payload = await response.json();
+      return typeof payload.data.id !== 'undefined' && payload.data.email === 'ceo@impactium.dev';
+    })
+    .catch(_ => false)
+    : false;
 
   if (!isLoggedIn && pathname.startsWith('/dashboard')) {
     return NextResponse.redirect(new URL('/auth/login', req.url));
