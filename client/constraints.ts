f0f5297a1efe6a7523d66de22e9624bb41651d82
MiregX <markgerasimchuk8@gmail.com>
2025-10-16 02:52:42 +0200

    _

--- client/constraints.ts
+++ client/constraints.ts
@@ -1,4 +1,6 @@
-export const HOST = process.env.HOST // production only usage
+import { APP_CONFIG } from "@/config/app-config";
+
+export const HOST = APP_CONFIG.host;
 export const SERVER_SSR = process.env.NODE_ENV === 'production' ? 'api:1337' : 'localhost:1337';
 export const SERVER_CSR = process.env.NODE_ENV === 'production' ? `https://${process.env.HOST}` : `http://${SERVER_SSR}`;
 export const Authorization = 'Authorization'
--- client/src/config/app-config.ts
+++ client/src/config/app-config.ts
@@ -5,6 +5,7 @@ const currentYear = new Date().getFullYear();
 export const APP_CONFIG = {
   name: "Impactium Metrics",
   version: packageJson.version,
+  host: 'metrics.impactium.dev',
   copyright: `© ${currentYear}, Impactium Metrics.`,
   meta: {
     title: "Impactium Metrics - метрики скорости и производительности инфраструктуры Impactium",
